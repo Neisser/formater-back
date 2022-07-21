ffa913fe3fe0a6ca7e412539751564887384f202
Neisser <villa.neisser@gmail.com>
2022-07-21 17:34:11 -0500

    chore(files): files/data response format changed

--- src/files/files.controller.js
+++ src/files/files.controller.js
@@ -6,9 +6,8 @@ async function getData(req, res, next) {
     const filedata = await getMultipleFileData(files, getFileData);
     const processedData = processData(filedata);
 
-    res.status(200).json({success: true, data:processedData})
+    res.status(200).json(processedData)
   } catch (err) {
-    console.log(err)
     res.status(400).json({
       success: false,
       err
